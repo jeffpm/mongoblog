e3e3be403fd55d510af33f9dc2cfeb3330043079
jzeitz <jessica.zeitz@gmail.com>
2010-04-21 20:11:52 -0400

    added links to login and register

--- header.php
+++ header.php
@@ -1,2 +1,8 @@
-<div id="header"><H1><a href="./">mongoblog</a></H1>
+<div id="header">
+<table width=780px>
+<tr><td width="70%"><H1><a href="./">mongoblog</a></H1></td>
+
+<td width="15%"><a href="./login.php">login</a></td>
+<td width="15%"><a href="./register.php">register</a></td></tr>
+</table>
 </div>
