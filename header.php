2b05c05097c79c66958a1b4a5db6a7fd47b70156
jzeitz <jessica.zeitz@gmail.com>
2010-04-21 21:03:23 -0400

    added pictures :)

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <div id="header">
 <table width=780px>
-<tr><td width="70%"><H1><a href="./">mongoblog</a></H1></td>
+<tr><td width="70%"><H1><a href="./">mangoblog</a></H1></td>
 <td width="15%"><a href="./login.php">login</a></td>
 <td width="15%"><a href="./register.php">register</a></td></tr>
 </table>
