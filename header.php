1431a4b06464376b8e6b4d65df21b1b69cc26464
jzeitz <jessica.zeitz@gmail.com>
2010-04-21 20:18:02 -0400

    updated links

--- header.php
+++ header.php
@@ -1,7 +1,6 @@
 <div id="header">
 <table width=780px>
 <tr><td width="70%"><H1><a href="./">mongoblog</a></H1></td>
-
 <td width="15%"><a href="./login.php">login</a></td>
 <td width="15%"><a href="./register.php">register</a></td></tr>
 </table>
