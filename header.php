3aea76915c74c200769bc44db6ee824940cd9d14
crandles <crandles@mail.umw.edu>
2010-04-21 23:30:08 -0400

    changed links when logged in, slight CSS changes

--- header.php
+++ header.php
@@ -1,7 +1,15 @@
 <div id="header">
 <table width=780px>
+<?php
+if(isset($_SESSION['username'])){
+	?> 
+<tr><td width="70%"><H1><a href="./">mangoblog</a></H1></td>
+<td width="15%"><a href="./postblog.php">compose</a></td>
+<td width="15%"><a href="./logout.php">logout</a></td></tr>
+<?php }else{ ?>
 <tr><td width="70%"><H1><a href="./">mangoblog</a></H1></td>
 <td width="15%"><a href="./login.php">login</a></td>
 <td width="15%"><a href="./register.php">register</a></td></tr>
+<?php }?>
 </table>
 </div>
